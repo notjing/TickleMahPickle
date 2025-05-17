309e10e19198c6c60f8cbc7081859db422e0f0ba
Andrew Chen <andrew07.chen@gmail.com>
2025-05-17 00:55:50 -0400

    navbar styled

--- ticklemypickle_frontend/src/App.js
+++ ticklemypickle_frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
+import Jars from './pages/Jars';
+import Profile from './pages/Profile';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="dashboard" element={<Dashboard />} />
+          <Route path="jars" element={<Jars/>} />
+          <Route path="profile" element={<Profile/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
