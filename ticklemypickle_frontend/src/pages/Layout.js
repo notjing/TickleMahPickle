bba6893b6a3b99f899692c46a6dccb53bf65b476
Kaibo Huang <99372604+KaiboHuang1@users.noreply.github.com>
2025-05-17 10:41:50 -0400

    navbar

--- ticklemypickle_frontend/src/pages/Layout.js
+++ ticklemypickle_frontend/src/pages/Layout.js
@@ -1,19 +1,20 @@
-
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import './Layout.css'; 
 
 const Layout = () => {
   return (
-    
     <div className="layout">
       <nav className="sidebar">
         <ul>
-          <li class="title"><Link to="/dashboard">TMP Dashboard</Link></li>
+          <li style={{ textAlign: 'center', marginBottom: '2rem' }}>
+            <Link to="/dashboard">
+              <img className='nav-img' src="./logo.png" alt="TickleMahPickle Logo" style={{ width: '90px', height: '90px', borderRadius: '16px', boxShadow: '0 2px 8px rgba(83,125,93,0.15)', objectFit: 'contain' }} />
+            </Link>
+          </li>
+          <li className="title"><Link to="/dashboard">TMP Dashboard</Link></li>
           <li><Link to="/Jars">Pickle Jars</Link></li>
           <li><Link to="/Profile">Profile</Link></li>
-
-
         </ul>
       </nav>
       <main className="content">
